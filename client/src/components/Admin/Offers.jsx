9ad490604be2b0d9e3413b8ec9845f51a5af9e1f
David <luisdavid.0698@hotmail.com>
2022-10-12 15:51:53 -0500

    offers admin styles

--- client/src/components/Admin/Offers.jsx
+++ client/src/components/Admin/Offers.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import {  getProducts, updateProduct } from "../../redux/actions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AddOffer from "./AddOffer";
 
 const Offers = () =>{
@@ -13,17 +13,29 @@ const Offers = () =>{
     dispatch(updateProduct({imageOffer:null},id))
   }
 
+  const [show, setShow] = useState(true)
+  let showIcon = false
+
+  if(window.screen.width < 777) showIcon = true
+  
+  
   useEffect(()=>{
+    if(window.screen.width < 777) setShow(false)
     dispatch(getProducts())
   },[refresh])
 
 	return(
     <div className="text-center">
 		  <div className="row mx-0">
-        <div className="col-md-4 mt-1 py-2 text-white bg-global">
-          <AddOffer/>
+        <div className="col-md-4 mt-2 py-2 text-white bg-global">
+          {showIcon &&
+            <i class="bi bi-text-center text-white h1" onClick={()=> setShow(!show)}></i>
+          }
+          {show &&       
+            <AddOffer/>
+          }
         </div>
-        <div className="col">
+        <div className="col mb-5">
           <div className=" product-list-offers-admin">
             {products && products.map(e=>{
               return(
